dc2150cadb5401eb2c97647f0e526f3db71a68ba
draet10 <shivamkr.vatsa@gmail.com>
2020-06-17 12:27:33 +0530

    Second Components

--- Module1/confus/src/App.js
+++ Module1/confus/src/App.js
@@ -3,8 +3,15 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      dishes: DISHES
+    };
+  }
   render(){
     return (
       <div >
@@ -13,8 +20,7 @@ class App extends Component{
             <NavbarBrand href="/">Harry Potter</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
-  
+        <Menu dishes={this.state.dishes}/> 
       </div>
     );
   }
