ceac7ea7015128008a9d798c8fd8d95624eb3304
draet10 <shivamkr.vatsa@gmail.com>
2020-06-17 18:31:02 +0530

    Updates

---
+++ precache-manifest.cd6f2c35f433b115f67f38b5d6d93b74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6813ec94529f7ca67dc1764689b5a1bd",
+    "url": "/CouseraReact/Module1/confus/index.html"
+  },
+  {
+    "revision": "b0341c9d69cb35a6a4f6",
+    "url": "/CouseraReact/Module1/confus/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "5ceb05be9c80368ed60a",
+    "url": "/CouseraReact/Module1/confus/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b0341c9d69cb35a6a4f6",
+    "url": "/CouseraReact/Module1/confus/static/js/2.37d07c91.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/CouseraReact/Module1/confus/static/js/2.37d07c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ceb05be9c80368ed60a",
+    "url": "/CouseraReact/Module1/confus/static/js/main.6419c584.chunk.js"
+  },
+  {
+    "revision": "e1a496405ed59d893188",
+    "url": "/CouseraReact/Module1/confus/static/js/runtime-main.5284878d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CouseraReact/Module1/confus/static/media/logo.5d5d9eef.svg"
+  }
+]);
