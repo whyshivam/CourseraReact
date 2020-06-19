47be5b01e9d1bd3e2406f5d95013aabb8f85f722
draet10 <shivamkr.vatsa@gmail.com>
2020-06-19 20:13:12 +0530

    single page App

--- Module1/confus/src/components/MainComponent.js
+++ Module1/confus/src/components/MainComponent.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import Dishdetail from './DishDetailComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
 import {Switch , Route, Redirect} from 'react-router-dom';
+
+
 class Main extends Component{
   constructor(props){
     super(props);
     this.state={
-      dishes: DISHES,
-      selectedDish: null
+			dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
-  onSelectedDish(dishId) {
-    this.setState({selectedDish: dishId});
-}
+	onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId});
+  }
+
   render(){
 		const HomePage =() => {
 			return (
-				<Home/>
+				<Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
 			);
 
 		}
@@ -28,10 +41,12 @@ class Main extends Component{
       <div >
         <Header/>
         <div className="container">
+					
            <Switch>
                <Route path="/home" component={HomePage}/>
 								<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
-                <Redirect to="/home" />
+								<Route exact path="/contactus" component={Contact} />
+								<Redirect to="/home" />
             </Switch>
         </div>
         <Footer/>
--- Module1/confus/src/components/MenuComponent.js
+++ Module1/confus/src/components/MenuComponent.js
@@ -1,34 +1,36 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
 
-
-
-//function RenderMenuItem(props)
-function RenderMenuItem({dish, onClick}){
-    return(
-        <Card onClick={() => onClick(dish.id)} >
-        <CardImg src={dish.image} alt={dish.name}></CardImg>
-        {console.log(dish.image)}
-        <CardImgOverlay>
-            <CardTitle >{dish.name}</CardTitle>
-        </CardImgOverlay>
-        </Card>
-    );
-}
-const Menu= (props) => {
-    const menu = props.dishes.map(dish => {
+    function RenderMenuItem ({dish, onClick}) {
         return (
-            <div key={dish.id} className='col-12 col-md-5 m-1'>
-               <RenderMenuItem dish={dish} onClick={props.onClick} />
+            <Card
+                onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
+    }
+
+    const Menu = (props) => {
+
+        const menu = props.dishes.map((dish) => {
+            return (
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
             </div>
         );
-    });
-    return (
-            <div className='row'>
-                {menu}
-            </div>
-        
-    );
-}
-    
-export default Menu; 
+    }
+
+export default Menu;
