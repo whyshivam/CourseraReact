fa166e9df218c8725186b431c621183e20f2d3e1
draet10 <shivamkr.vatsa@gmail.com>
2020-06-20 00:57:01 +0530

    About US ADDed

--- Module1/confus/src/components/MainComponent.js
+++ Module1/confus/src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import Dishdetail from './DishDetailComponent'
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -59,6 +60,7 @@ class Main extends Component{
 								<Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
 								<Route path="/menu/:dishId" component ={DishWithID} />
 								<Route exact path="/contactus" component={Contact} />
+								<Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
 								<Redirect to="/home" />
             </Switch>
         </div>
