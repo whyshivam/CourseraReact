3f0155a0e31c49e31198f73ae55efbbbd84a54e9
draet10 <shivamkr.vatsa@gmail.com>
2020-06-23 16:25:30 +0530

    React Redux Form Validation

--- Module1/confus/src/components/ContactComponent.js
+++ Module1/confus/src/components/ContactComponent.js
@@ -2,13 +2,18 @@ import React,{Component} from 'react';
 import { Breadcrumb, BreadcrumbItem, Button,Label, Input, Col, Row } from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
+const required =(val) => val && val.length;
+const maxLength=(len) => (val) => !(val) || (val.length <= len);
+const minLength=(len) => (val) => !(val) || (val.length >= len);
+const isNumber =(val) => !isNaN(Number(val));
+const validEmail =(val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
+
 class Contact extends Component {
 	constructor(props) {
 		super(props);
 
-		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleBlur =this.handleBlur.bind(this);
 }
 handleSubmit(values) {
 		console.log('Current State is: ' + JSON.stringify(values));
@@ -71,6 +76,16 @@ handleSubmit(values) {
                                     <Control.text model=".firstname" id="firstname" name="firstname"
                                         placeholder="First Name"
                                         className="form-control"
+                                        validators={{required,minLength: minLength(3),maxLength : maxLength(15)}}
+                                         />
+                                         <Errors className="text-danger"
+                                            model=".firstname"
+                                            show ="touched"
+                                            messages={{
+                                                required : 'Required',
+                                                minLength : 'Must be atleast 3',
+                                                maxLength :' maximum 15 char'
+                                            }}
                                          />
                                 </Col>
                             </Row>
@@ -80,6 +95,16 @@ handleSubmit(values) {
                                     <Control.text model=".lastname" id="lastname" name="lastname"
                                         placeholder="Last Name"
                                         className="form-control"
+                                        validators={{required,minLength: minLength(3),maxLength : maxLength(15)}}
+                                         />
+                                         <Errors className="text-danger"
+                                            model=".lastname"
+                                            show ="touched"
+                                            messages={{
+                                                required : 'Required',
+                                                minLength : 'Must be atleast 3',
+                                                maxLength :' maximum 15 char'
+                                            }}
                                          />
                                 </Col>
                             </Row>
@@ -89,7 +114,18 @@ handleSubmit(values) {
                                     <Control.text model=".telnum" id="telnum" name="telnum"
                                         placeholder="Tel. Number"
                                         className="form-control"
-                                         />
+                                        validators={{required,minLength: minLength(3),maxLength : maxLength(15), isNumber}}
+                                        />
+                                        <Errors className="text-danger"
+                                           model=".telnum"
+                                           show ="touched"
+                                           messages={{
+                                               required : 'Required',
+                                               minLength : 'Must be atleast 3',
+                                               maxLength :' maximum 15 char',
+                                               isNumber : 'must be number'
+                                           }}
+                                        />
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -97,7 +133,13 @@ handleSubmit(values) {
                                 <Col md={10}>
                                     <Control.text model=".email" id="email" name="email"
                                         placeholder="Email"
-                                        className="form-control" />
+                                        className="form-control" 
+                                        validators={{required,validEmail}}/>
+                                        <Errors className="text-danger" model=".email" show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            validEmail : 'Must be valid email'
+                                        }}/>
                                 </Col>
                             </Row>
                             <Row className="form-group">
@@ -123,7 +165,7 @@ handleSubmit(values) {
                                 <Label htmlFor="message" md={2}>Your Feedback</Label>
                                 <Col md={10}>
                                     <Control.textarea model=".message" id="message" name="message"
-                                        rows="12"
+                                       
                                         className="form-control" />
                                 </Col>
                             </Row>
