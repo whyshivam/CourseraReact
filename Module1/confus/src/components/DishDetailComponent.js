520c8276e667d7aba5afc54783f5e59447f11c09
Shivam Kumar <shivamkr.vatsa@gmail.com>
2020-06-30 20:10:15 +0530

    redux-thunk and logger

--- Module1/confus/src/components/DishDetailComponent.js
+++ Module1/confus/src/components/DishDetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardBody, CardText, Breadcrumb, BreadcrumbItem,Button,Mo
      Label, Input,Col,Row} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
-
+import {Loading} from './LoadingComponent';
 const maxLength=(len) => (val) => !(val) || (val.length <= len);
 const minLength=(len) => (val) => !(val) || (val.length >= len);
 //User Defined components start with caps
@@ -138,32 +138,53 @@ class Commentform extends Component{
     }
 
     const Dishdetail=(props) => {
-        if (props.dish == null) {
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish == null) {
             return (<div></div>);
         }
-        return (
-            <div className="container">
-                <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3> {props.dish.name}</h3>
-                    <hr/>
+        else{
+            return (
+                <div className="container">
+                    <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3> {props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                    </div>
+                    <div className='row'>
+                        <RenderDish dish={props.dish}/>
+                        <RenderComments comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}
+          />
+    
+                        
+                    </div>
                 </div>
-                </div>
-                <div className='row'>
-                    <RenderDish dish={props.dish}/>
-                    <RenderComments comments={props.comments}
-        addComment={props.addComment}
-        dishId={props.dish.id}
-      />
-
-                    
-                </div>
-            </div>
-        );
+            );
+        }
+        
     }
 
 
--- Module1/confus/src/components/MenuComponent.js
+++ Module1/confus/src/components/MenuComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import {Loading} from './LoadingComponent';
 
 //`` back quotes adresses are implemented for such queries
     function RenderMenuItem ({dish}) {
@@ -19,7 +20,7 @@ import { Link } from 'react-router-dom';
 
     const Menu = (props) => {
 
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
                     <RenderMenuItem dish={dish}  />
@@ -27,25 +28,46 @@ import { Link } from 'react-router-dom';
             );
         });
 
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem>
-                            <Link to='/home'>Home</Link> 
-                        </BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3> Menu</h3>
-                        <hr/>
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
                     </div>
                 </div>
-                <div className="row">
-                    {menu}
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row"> 
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        else{
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to='/home'>Home</Link> 
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3> Menu</h3>
+                            <hr/>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {menu}
+                    </div>
+                </div>
+        );}
     }
 
 export default Menu;
