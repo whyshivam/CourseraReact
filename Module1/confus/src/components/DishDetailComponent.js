263dc461645a48f3260927a0429b50b9eabe64db
Shivam Kumar <shivamkr.vatsa@gmail.com>
2020-07-04 17:42:09 +0530

    react-animation-components

--- Module1/confus/src/components/DishDetailComponent.js
+++ Module1/confus/src/components/DishDetailComponent.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 import {Loading} from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+//@ts-ignore
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const maxLength=(len) => (val) => !(val) || (val.length <= len);
 const minLength=(len) => (val) => !(val) || (val.length >= len);
@@ -95,33 +97,42 @@ class Commentform extends Component{
     function RenderComments({comments,postComment,dishId}) {
         if (comments == null) {
             return (<div></div>)
-        }
-        console.log("RenderComments"+ comments);
-        const cmnts = comments.map(comment => {
-            return (
-                <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                    </p>
-                </li>
-            )
-        })
+        }else{
+            console.log("RenderComments"+ comments);
+            const cmnts = comments.map(comment => {
+                return (
+                    <Fade in>
+                        <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author},
+                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                            </p>
+                        </li>
+                    </Fade>
+                )
+            })
         return (
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
-                    {cmnts}
+                    <Stagger in>
+                        {cmnts}
+                    </Stagger>
                 </ul>
                 <Commentform dishId={dishId} postComment={postComment} />
             </div>
-        )
+        )}
     }
 
     function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
+                    <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
                     <Card>
                         <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
@@ -129,6 +140,7 @@ class Commentform extends Component{
                             <CardText>{dish.description}</CardText>
                         </CardBody>
                     </Card>
+                    </FadeTransform>
                 </div>
             );
         }else{
--- Module1/confus/src/components/HomeComponent.js
+++ Module1/confus/src/components/HomeComponent.js
@@ -3,6 +3,9 @@ import { Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle} from 'reactstrap';
 import {Loading} from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+// @ts-ignore
+import { FadeTransform } from 'react-animation-components';
+
 
 function RenderCard({item, isLoading, errMess}) {
     
@@ -18,14 +21,20 @@ function RenderCard({item, isLoading, errMess}) {
     }
     else 
         return(
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-                <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                    <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
         );
 
 }
